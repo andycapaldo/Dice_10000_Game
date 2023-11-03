65e85c03ff3935f678504eca12ee1dc0b281f73d
Andrew Capaldo <andycapaldo2@gmail.com>
2023-11-03 15:20:59 -0600

    Added scoring logic and roll count to display

--- static/js/index.js
+++ static/js/index.js
@@ -12,7 +12,6 @@ function pageLoader(){
 
 // Function that rolls the dice
 function rollDice(e){
-    console.log('Clicked Roll Dice button');
     console.log(e.target.value);
     if (e.target.value === 'Roll'){
         displayDice();
@@ -28,21 +27,92 @@ function getRandomNumber() {
 }
 
 
-// Callback function for rollDice that will add dice rolls to the page
+// Callback function for rollDice that will add dice rolls to the page and update round and total scores
 function displayDice(){
     let rolledDice = document.getElementById('rolled-dice');
-    clearGame(rolledDice);
-    for (i=0; i < 6; i++){
+    let roundScore = document.getElementById('round-score');
+    let totalScore = document.getElementById('total-score');
+    let rollCount = document.getElementById('roll-number')
+    let diceValues = [];
+
+    let roundTotal = 0;
+
+    rolledDice.innerHTML = '';
+    for (let i=0; i < 6; i++){
         let die = document.createElement('p');
         let randomNumber = getRandomNumber();
+        diceValues.push(randomNumber);
+
         die.textContent = 'Roll ' + (i + 1) + ': ' + randomNumber;
         die.classList.add('rolled-die');
         rolledDice.appendChild(die);
     }
+    console.log(diceValues);
+    roundTotal = calcScore(diceValues)
+    roundScore.textContent = roundTotal;
+    totalScore.textContent = parseInt(totalScore.textContent) + roundTotal;
+    rollCount.textContent = parseInt(rollCount.textContent) + 1;
+    
+    if (parseInt(totalScore.textContent) >= 10000){
+        console.log('You win!!!');
+    }
 }
 
 
-// Function to clear the current dice as well as the round and total scores
-function clearGame(rolledDice=rolledDice){
-    rolledDice.innerHTML = '';
+// Function to clear and reset the game
+function clearGame(){
+    return;
+}
+
+
+// Function with the game logic to determine the score of a roll of 6 dice
+function calcScore(diceValues){
+    let score = 0;
+    if (isStraight(diceValues)){
+        score += 1500;
+        return score;
+    }
+    // Count the # of triples
+    const countMap = new Map();
+    for (const value of diceValues) {
+        if (countMap.has(value)) {
+            countMap.set(value, countMap.get(value) + 1);
+        } else {
+            countMap.set(value, 1);
+        }
+    }
+    // Calculate the score for each triple
+    for (const [value, count] of countMap.entries()){
+        if (count >= 3) {
+            score += calculateTripleScore(value);
+        }
+    }
+    for (const value of diceValues) {
+        if (value === 1) {
+            score += 100;
+        } else if (value === 5) {
+            score += 50;
+        }
+    }
+    return score;
+}
+
+
+// Function to check if the roll was a straight 
+function isStraight(diceValues){
+    // Sort the array in ascending order (function is used to ensure the values are sorted numerically and not as strings)
+    diceValues.sort((a, b) => a - b);
+    return JSON.stringify(diceValues) === JSON.stringify([1, 2, 3, 4, 5, 6]);
+}
+
+
+// Function to calculate the score if a triple is present
+function calculateTripleScore(tripleValue){
+    if (tripleValue === 1) {
+        return 1000; // Score for 3 1's
+    } else if (tripleValue >= 2 && tripleValue <= 5) {
+        return tripleValue * 100;
+    } else {
+        return 0;
+    }
 }
